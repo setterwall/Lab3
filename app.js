b6704e090e25322bba328d673c30b70d59d7931a
stralf <annika.stralfors@gmail.com>
2015-04-28 18:09:15 +0200

    updated app with geolocation mm

--- app.js
+++ app.js
@@ -1,47 +1,96 @@
+var alias;
+var geocoder;
+
 function sendMsg() {
-	var msg = document.getElementById('textInput').value;
+	var msg = alias+ " -->  " +document.getElementById('textInput').value;
 	chatChannel.publish({channel: 'Sandbox', message : msg});
 	document.getElementById('textInput').value = '';
-        }
+}
+
+function newUser() {
+	 geocoder = new google.maps.Geocoder();
+	 getLocation();
+}
 
 function logOut() {
 	chatChannel.unsubscribe({channel : 'Sandbox'});
 	showView('homeView');
 	document.getElementById('chatWindow').value = '';
-        }
+	document.getElementById('userName').value = '';
+	document.getElementById('messageText').value = '';
+}
 
 function loadChat(user){
-	if(user == 'read') {
+	/**if(user == 'read') {
 		window.chatChannel = PUBNUB.init({
 		subscribe_key: 'sub-c-ee7c4d30-e9ba-11e4-a30c-0619f8945a4f'
-	});
+	});*/
 
-		chatChannel.subscribe({
-		channel: 'Sandbox',
-		message: function(m){document.getElementById('chatWindow').value = m + '\n' + document.getElementById('chatWindow').value},
-		connect: function(){console.log("Connected")},
-		disconnect: function(){console.log("Disconnected")},
-		reconnect: function(){console.log("Reconnected")},
-		error: function(){console.log("Network Error")},         
- 	});
+	alias = document.getElementById('userName').value;
+
+	if (user == 'readwrite') {
+		if(alias == ""){
+		  var message = document.getElementById('messageText');
+			message.innerHTML = "Enter an alias!";
+		}
+
+		else{
+			document.getElementById('chatWindow').value = '';
+			var randomID = PUBNUB.uuid();
+			window.chatChannel = PUBNUB.init({
+			publish_key: 'pub-c-c9b9bd43-e594-4146-b78a-716088b91de8',
+			subscribe_key: 'sub-c-ee7c4d30-e9ba-11e4-a30c-0619f8945a4f',
+			uuid: randomID
+			});
+
+			chatChannel.subscribe({
+		      channel: 'Sandbox',
+		      message: function(m){document.getElementById('chatWindow').value = m + '\n' + document.getElementById('chatWindow').value},
+		      connect: function(){console.log("Connected")},
+		      disconnect: function(){console.log("Disconnected")},
+		      reconnect: function(){console.log("Reconnected")},
+		      error: function(){console.log("Network Error")},         
+	 	});
+
+		showView('chatView');   
+		newUser();
+		}
+
+	}
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        alert("Geolocation is not supported by this browser.");
     }
+}
 
-	else if (user == 'readwrite') {
-		window.chatChannel = PUBNUB.init({
-		publish_key: 'pub-c-c9b9bd43-e594-4146-b78a-716088b91de8',
-		subscribe_key: 'sub-c-ee7c4d30-e9ba-11e4-a30c-0619f8945a4f'
-		});
-		chatChannel.subscribe({
-        channel: 'Sandbox',
-        message: function(m){document.getElementById('chatWindow').value = m + '\n' + document.getElementById('chatWindow').value},
-        connect: function(){console.log("Connected")},
-        disconnect: function(){console.log("Disconnected")},
-        reconnect: function(){console.log("Reconnected")},
-        error: function(){console.log("Network Error")},         
- 	});        
-    }
+function showPosition(position) {
+	 convertLocation(position.coords.latitude, position.coords.longitude);	
+}
 
-    showView('chatView');
+function convertLocation(lat, lng) {
+
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        if (results[1]) {
+         outputLocation(results[0].formatted_address)
+
+        } else {
+          alert("No results found");
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  }
+
+function outputLocation(tmp){
+			var msg = "New guest! '" +alias+"' enter the chatroom from "+tmp;
+			chatChannel.publish({channel: 'Sandbox', message : msg});
 }
 
 //Toggle between the different views
@@ -53,5 +102,4 @@ function showView(goto) {
     var next = document.getElementById(goto);
     next.style.display = 'block';
 }
-
 
